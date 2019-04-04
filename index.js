c41b4fb45eeb4e6c4817dad7b99dc5bf4f5a9eae
artiom <artem@MacBook-Pro-Artem.local>
2019-04-04 15:49:46 +0300

    lesson19

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("result").addEventListener("click", myFunction, myDunction2, myFunction3);
+
+function myFunction() {
+    document.getElementById("result").innerHTML = "You clicked First button";
+}
+
+function myFunction2() {
+    document.getElementById("result").innerHTML = "You clicked Second button";
+}
+
+function myFunction3() {
+    document.getElementById("result").innerHTML = "You clicked Third button";
+} 
